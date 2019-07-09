3e19c85a3986ad5b621adc99ca7fb30b8f57b12e
JacqueStar3 <JacqueStar3@github.com>
2019-07-09 15:09:47 -0500

    Design Patterns Base

---
+++ src/com/designpatterns/base/Touring.java
@@ -0,0 +1,18 @@
+package com.designpatterns.base;
+
+import com.designpatterns.java.BikeColor;
+
+public class Touring extends RoadBike {
+
+    public Touring(WheelInterface wheel, BikeColor color){
+        super(wheel, color);
+    }
+
+
+   public Touring(WheelInterface wheel) {
+        this(wheel, BikeColor.BLUE);
+   }
+
+
+
+}
